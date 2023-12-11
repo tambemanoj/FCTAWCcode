79ab8e18fef8898c676f92f2d0a3ac30bc2125e1
manoj FCT <manojd.tambe@gmail.com>
2023-12-11 19:43:40 +0530

    First commit

---
+++ DemoTwo/src/main/java/demo/LearnNewThings/App.java
@@ -0,0 +1,34 @@
+package demo.LearnNewThings;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main( String[] args )
+    {
+		RemoteWebDriver driver = null;
+
+    	
+    	int age = 19      ;
+    	
+    if (age>18){
+    	System.out.println("yes");
+    }
+    else if (age<18) {
+		System.out.println("no");
+	}
+    
+	
+    	
+    }
+}
